c32f45c2bf24d3aca45cb2bb227f189bb1e3d106
Alex <odinale@ukr.net>
2022-01-17 08:59:12 +0200

    Added "Yandex.Metrika informer" & "Yandex.Metrika counter"

--- wp-content/themes/we-are-for-teachers/header.php
+++ wp-content/themes/we-are-for-teachers/header.php
@@ -12,6 +12,42 @@
   <link rel="stylesheet" href="<?php bloginfo( 'template_directory' ) ?>/styles/slick.css">
   <link rel="stylesheet" href="<?php bloginfo( 'template_directory' ) ?>/styles/main.css">
   <title><?php bloginfo( 'name' ) ?> | <?php bloginfo( 'description' ) ?></title>
+
+  <!-- Yandex.Metrika informer -->
+  <a href="https://metrika.yandex.ru/stat/?id=87156109&amp;from=informer"
+     target="_blank" rel="nofollow">
+    <img src="https://informer.yandex.ru/informer/87156109/3_1_FFFFFFFF_EFEFEFFF_0_pageviews"
+         style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика"
+         title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)"
+         class="ym-advanced-informer" data-cid="87156109" data-lang="ru"/>
+  </a>
+  <!-- /Yandex.Metrika informer -->
+
+  <!-- Yandex.Metrika counter -->
+  <script type="text/javascript">
+    (function (m, e, t, r, i, k, a) {
+      m[i] = m[i] || function () {
+        (m[i].a = m[i].a || []).push(arguments)
+      };
+      m[i].l = 1 * new Date();
+      k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+    })
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    ym(87156109, "init", {
+      clickmap: true,
+      trackLinks: true,
+      accurateTrackBounce: true,
+      webvisor: true
+    });
+  </script>
+  <noscript>
+    <div>
+      <img src="https://mc.yandex.ru/watch/87156109" style="position:absolute; left:-9999px;" alt=""/>
+    </div>
+  </noscript>
+  <!-- /Yandex.Metrika counter -->
+
 </head>
 
 <body>
